e92af8d05fce5c46125518ced272ce74674a66ad
gmurad97 <murad.dev@bk.ru>
2024-04-26 04:59:17 +0400

    first steps

---
+++ src/components/classable.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+}
