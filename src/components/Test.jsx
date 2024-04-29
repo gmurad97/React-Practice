94dd94e3a2711b78b46e101e368383a23ad2894c
gmurad97 <murad.dev@bk.ru>
2024-04-29 04:29:10 +0400

    props & fetch

---
+++ src/components/Test.jsx
@@ -0,0 +1,6 @@
+export default function Test(props){
+    // console.log(props.innerTextProps);
+    return (
+        <h1>{props.innerTextProps}</h1>
+    );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,14 @@ import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
 import "./css/style.css"
+import Test from './components/Test';
+import BinanceData from './components/BinanceData';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BinanceData />
+    <Test innerTextProps="hi how are you" />
     <Header />
     <Main />
     <Footer />
