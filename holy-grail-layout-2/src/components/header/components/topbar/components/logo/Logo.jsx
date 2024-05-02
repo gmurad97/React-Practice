a6dd4ef31a2a5096e2d1212bb40aa8ef0be46c40
gmurad97 <murad.dev@bk.ru>
2024-05-02 17:32:24 +0400

    holy grail layout 2 add css hover

--- holy-grail-layout-2/src/components/header/components/topbar/components/logo/Logo.jsx
+++ holy-grail-layout-2/src/components/header/components/topbar/components/logo/Logo.jsx
@@ -2,10 +2,12 @@ import "./Logo.css";
 
 import LogoImg from "../../../../../../assets/img/logo.svg";
 
-const Logo = ()=>{
-    return(
+const Logo = () => {
+    return (
         <div className="logo">
-            <img src={LogoImg} className="logo__img" alt="Logo" />
+            <a href="/#" className="logo__link">
+                <img src={LogoImg} className="logo__img" alt="Logo" />
+            </a>
         </div>
     );
 }
