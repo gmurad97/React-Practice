7502f11d4cde95e5aac9ff47b71d5ce885c0969b
gmurad97 <murad.dev@bk.ru>
2024-05-06 04:13:10 +0400

    use state (counter)

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,18 @@
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+    const [counter, setCounter] = useState(0);
+
+    function counterInc() {
+        setCounter(counter + 1);
+    }
+
     return (
-        <h1>React Practice</h1>
+        <div className="app">
+            <h1>Counter:{counter}</h1>
+            <button type="button" onClick={counterInc}>Click Me!</button>
+        </div>
     );
 }
 
