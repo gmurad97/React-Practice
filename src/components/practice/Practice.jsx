53b7b3dea19a17fe617cbb87ae03dbb92341c700
gmurad97 <murad.dev@bk.ru>
2024-05-19 05:30:32 +0400

    useContext

--- src/components/practice/Practice.jsx
+++ src/components/practice/Practice.jsx
@@ -1,8 +1,38 @@
-import styles from "./Practice.module.css"
+import { createContext, useContext, useState } from "react";
+import { ThemeContext, ThemeProvider } from "./context/ThemeContext";
+
+const GlobalContext = createContext([]);
 
 const Practice = () => {
+    const [theme, setTheme] = useState("dark");
     return (
-        <h1 className={styles.module}>Practice CSS MODULE</h1>
+        <ThemeProvider>
+            <Component />
+        </ThemeProvider>
+    );
+}
+
+const Component = () => {
+    return (
+        <Component2 />
+    );
+}
+
+const Component2 = () => {
+    return (
+        <Component3 />
+    );
+}
+
+const Component3 = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext);
+
+
+    return (
+        <>
+            <h1>{theme}</h1>
+            <button onClick={() => toggleTheme()}>{theme}</button>
+        </>
     );
 }
 
