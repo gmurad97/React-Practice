b2fd0d7179b1fd3f2bff98531000f7f4282182d7
gmurad97 <murad.dev@bk.ru>
2024-05-20 11:02:40 +0400

    useRef

--- src/components/practice/Practice.jsx
+++ src/components/practice/Practice.jsx
@@ -1,9 +1,32 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useRef } from "react";
 import { ThemeContext, ThemeProvider } from "./context/ThemeContext";
 
 const GlobalContext = createContext([]);
 
+
 const Practice = () => {
+    //input element in useref
+
+    //if you need to change a static element, then use useref
+    //otherwise, if we change, add, or delete data from an object, for example (dynamic content) then we use useState
+    const inpRef = useRef(null);
+
+
+    const onButtonClick = () => {
+        //change state but without render component
+        inpRef.current.focus();
+        console.log(inpRef.current.value);
+    };
+
+    return (
+        <>
+            <input ref={inpRef} type="text" />
+            <button type="button" onClick={() => onButtonClick()}>ButtonRef</button>
+        </>
+    );
+}
+
+const PracticeContext = () => {
     const [theme, setTheme] = useState("dark");
     return (
         <ThemeProvider>
