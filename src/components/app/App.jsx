2bdb1ccf20c7bfaa5c4f63c8f7f3beda0258e912
gmurad97 <murad.dev@bk.ru>
2024-07-06 15:36:03 +0400

    Route params and useParams

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
                     <Route index element={<Home />} />
                     <Route path="home" element={<Home />} />
                     <Route path="shop" element={<Shop />} />
+                    <Route path="shop/:categoryName" element={<Shop />} />
                     <Route path="contacts" element={<Contacts />} />
                     <Route path="about" element={<About />} />
                 </Route>
--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -1,7 +1,10 @@
+import { useParams } from "react-router-dom";
+
 const Shop = () => {
+    const urlQuery = useParams();
     return (
         <div className="shop">
-            Shop Page
+            Shop Page - {urlQuery.categoryName}
         </div>
     );
 }
