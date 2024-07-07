4f1edec7df1f1c221d78eab82b2953adb0ef5016
gmurad97 <murad.dev@bk.ru>
2024-07-08 02:33:04 +0400

    rrd - useHref & breadcrumb

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -1,11 +1,25 @@
-import { useParams } from "react-router-dom";
+import { useHref, useParams } from "react-router-dom";
 
 const Shop = () => {
-    const { categoryName, productId } = useParams();
+    /* const { categoryName, productId } = useParams(); */
+    const currentUrlRelativePath = useHref();
+    const breadcrumb = currentUrlRelativePath.slice(1).split("/");
+
+
+    const breadcrumbConcats = breadcrumb.reduce((acc, current, index, arr) => {
+        if (index !== arr.length - 1)
+            acc += current + " > ";
+        else
+            acc += current;
+
+        return acc;
+    }, "Breadcrumb: ");
+
+    console.log(breadcrumbConcats);
 
     return (
         <div className="shop">
-            Shop Page - {typeof categoryName !== "undefined" && typeof productId === "undefined" ? "All " + categoryName + " Products" : categoryName + " Single Product " + productId}
+            Shop Page {/* - {typeof categoryName !== "undefined" && typeof productId === "undefined" ? "All " + categoryName + " Products" : categoryName + " Single Product " + productId} */}
         </div>
     );
 }
