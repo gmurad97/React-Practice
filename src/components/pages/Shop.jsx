3a1db429ef644cba0ed0e6912be5325f827310b3
gmurad97 <murad.dev@bk.ru>
2024-07-09 05:23:55 +0400

    rrd useLocation

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -1,4 +1,14 @@
+import { useLocation } from "react-router-dom";
+
 const Shop = () => {
+    let currentLocation = useLocation();
+    console.log(currentLocation)
+    console.log(currentLocation.pathname);
+    console.log(currentLocation.hash);
+    console.log(currentLocation.key);
+    console.log(currentLocation.search);
+    console.log(currentLocation.state);
+
     return (
         <div className="shop">
             Shop Page
--- src/components/pages/layout/MainLayout.jsx
+++ src/components/pages/layout/MainLayout.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../navbar/Navbar";
 
-const MainLayout = ({children}) => {
+const MainLayout = () => {
     return (
         <div className="container">
             <div className="row">
                 <Navbar />
             </div>
             <div className="row">
-                {children}
+                <Outlet />
             </div>
         </div>
     );
